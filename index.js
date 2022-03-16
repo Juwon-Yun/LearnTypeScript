665583b916745897d4961af4fa48a88d3e10b585
Juwon-Yun <eooll2@daum.net>
2022-03-16 12:16:58 +0900

    Feat: index.ts

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 // .ts 파일은 브라우저가 읽지 못한다 
 // ts파일을 js를 변환해서 사용함
-let name = 'juwon';
-name = 123;
+// types => string, number, boolean, null, undefined, bigint, [], {}등등
+var name2 = ['Yun', 'Juwon'];
+// union type
+var juwon = 1;
+var juwon2 = ['juwon', 1];
+function multiple(x) {
+    return x * 2;
+}
+var john = [123, true];
+var park = { name: 'asdfasdf', age: '123123' };
+// class type 
+var User = /** @class */ (function () {
+    function User(name) {
+        this.name = name;
+    }
+    return User;
+}());
+// type 문법이외의 기능도 많다.
--- index.ts
+++ index.ts
@@ -1,6 +1,40 @@
 // .ts 파일은 브라우저가 읽지 못한다 
 // ts파일을 js를 변환해서 사용함
 
-let name: string = 'juwon'
+// types => string, number, boolean, null, undefined, bigint, [], {}등등
 
-name = 123
+const name2: string[] = ['Yun', 'Juwon']
+
+// union type
+const juwon:string | number = 1
+
+// type alias( uppercase )
+type MyType = string | number
+
+let juwon2 :MyType[] = ['juwon', 1]
+
+function multiple(x :number) :number {
+    return x * 2
+}
+
+// tuple type
+type Member = [number, boolean];
+let john: Member = [123, true]
+
+// object type 속성이 많은 경우
+type Member2 = {
+    // string으로 된 모든 object 속성의 타입은 'string'
+    [key :string] : string
+}
+
+let park: Member2 = { name: 'asdfasdf', age: '123123' }
+
+// class type 
+class User { 
+    name :string;
+    constructor(name :string) {
+        this.name = name
+     }
+}
+
+// type 문법이외의 기능도 많다.
