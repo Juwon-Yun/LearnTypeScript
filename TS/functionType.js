1aef580e525cbbdcb95ad6fc3ae78e7be43555af
Juwon-Yun <eooll2@daum.net>
2022-03-16 15:09:10 +0900

    Feat: Void Type, Narrowing

---
+++ TS/functionType.js
@@ -0,0 +1,15 @@
+function multi(x) {
+    return x * 2;
+}
+multi(30);
+// 함수에서 void 타입 활용 가능 ( 아무것도 리턴하지 않음 ) 
+// 타입이 지정된 파라미터는 필수 입력 값이다.
+// 파라미터가 옵션인 경우엔 변수?: 타입 
+// 변수?:number는 변수:number|undefi
+function voidfunc(x) {
+}
+function quiz(x) {
+    // type Narrowing
+    // union type에서 type을 지정하는 방법
+    console.log(+x + 3);
+}
