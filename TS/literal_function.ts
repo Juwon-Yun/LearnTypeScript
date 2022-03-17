3c28076e3832301c1a322862ab4127db39328f71
Juwon-Yun <eooll2@daum.net>
2022-03-17 12:15:15 +0900

    Feat: Literal Function

---
+++ TS/literal_function.ts
@@ -0,0 +1,18 @@
+type functionType = (a: string) => number;
+
+let myFunc:functionType = function (a) { 
+    return +a
+}
+
+// object 안에 함수 만들기
+let memberInfo = {
+    name: 'kim',
+    // 파라미터가 있는데 타입지정안하면 에러
+    pluseOne(a : number) :number{ 
+        return a + 1
+    },
+    changeName: () => { }
+}
+
+memberInfo.pluseOne(1)
+
