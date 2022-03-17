95105dc9eb05cc26f5bee6738c4fa97acb338aa5
Juwon-Yun <eooll2@daum.net>
2022-03-17 15:10:23 +0900

    Feat: JSX.Element, Components, Props TypeScript

--- reat_ts/src/App.tsx
+++ reat_ts/src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let a: string = 'a에용'
+
+// div라는 html요소로 타입지정
+let box: JSX.IntrinsicElements['div'] = <div>{a}</div>
+
 function App() {
+
+  // 타입지정 auto
+  let [user, setUser] = useState<string | number>('kim')
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,9 +27,22 @@ function App() {
         >
           Learn React
         </a>
+        {box}
+        <Profile name="chulsu" age={20}></Profile>
       </header>
     </div>
   );
+}
+
+// props 때문에 에러가 많다.
+// 초기에 props의 타입을 잘 지정해놓으면 버그를 최소화할수있다.
+
+// components
+// props 타입지정 props는 object가 default 
+function Profile(props: {name : string, age : number}) :JSX.Element { 
+  return (
+    <div>프로필입니다.</div>
+  )
 }
 
 export default App;
